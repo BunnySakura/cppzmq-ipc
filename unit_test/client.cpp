74ddcbe0c7a835951d3ab3b73c9df05f207ce540
3rd <hello3rd@outlook.com>
2023-11-01 10:42:01 +0800

    test: 完善C++单元测试

--- unit_test/client.cpp
+++ unit_test/client.cpp
@@ -1,69 +1,55 @@
 #include "zmq_ipc.h"
-#include "thread"
 
+#include <string>
 #include <cstring>
+#include <csignal>
 
-static void callback_func(CZmqIpcString zmq_string, CZmqIpcByteArr zmq_bytes) {
-  printf("%s\t", zmq_string.string);
-  for (int i = 0; i < zmq_bytes.size; i++) {
-    printf("%02x ", *(zmq_bytes.byte_arr + i));
+static bool exit_flag = false;
+
+// 信号处理函数
+void SignalHandler(int signal) {
+  if (signal == SIGINT) {
+    printf("Ctrl+C pressed. Exiting...\n");
+    // 在这里执行退出操作
+    exit_flag = true;
   }
-  printf("\n");
 }
 
-static void callback_stress(CZmqIpcString zmq_string, CZmqIpcByteArr zmq_bytes) {
-  // 解析字节数组为秒数
-  uint64_t receive_time_in_seconds;
-  std::memcpy(&receive_time_in_seconds, zmq_bytes.byte_arr, sizeof(uint64_t));
-  // 将秒数转换为时间点
-  auto receive_time = std::chrono::time_point<std::chrono::system_clock>(
-      std::chrono::seconds(receive_time_in_seconds));
-  // 计算时间差，单位为秒
-  auto elapsed_time = std::chrono::system_clock::now() - receive_time;
-  double elapsed_seconds = std::chrono::duration<double>(elapsed_time).count();
+void CallbackFunc(const std::string &topic, const std::vector<uint8_t> &message) {
+  if (message != std::vector<uint8_t>({'w', 'o', 'r', 'l', 'd'})) {
+    // 解析字节数组为ms
+    uint64_t receive_time_in_ms;
+    std::memcpy(&receive_time_in_ms, message.data(), sizeof(uint64_t));
+    // 将时间转换为时间点
+    auto receive_time = std::chrono::system_clock::time_point(std::chrono::milliseconds(receive_time_in_ms));
+    // 计算时间差
+    auto elapsed_time = std::chrono::system_clock::now() - receive_time;
+    auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed_time).count();
 
-  printf("Elapse: %f s\n", elapsed_seconds);
+    printf("Elapse: %ld ms\n", elapsed_ms);
+  } else {
+    std::string str;
+    for (auto &c : message) {
+      str += static_cast<char>(c);
+    }
+    printf("Receive message: %s\n", str.c_str());
+  }
 }
 
 int main() {
-  ZmqIpc *zmq_ipc = CZmqIpcNew();
-#if 0
-  CZmqIpcInit(zmq_ipc, callback_func, 1,
-              {sizeof("tcp://localhost:5555"), "tcp://localhost:5555"},
-              {sizeof("tcp://localhost:5556"), "tcp://localhost:5556"});
-  CZmqIpcSubscribe(zmq_ipc, {sizeof("world"), "world"});
-  uint8_t idx = 0;
-  uint8_t msg[2] = {0};
-  while (idx != 0xff) {
-    msg[0] = idx++;
-    CZmqIpcPublish(zmq_ipc,
-                   {sizeof("hello"), "hello"},
-                   {sizeof(msg), msg});
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-  }
-#else
-  CZmqIpcInit(zmq_ipc, callback_stress, 8,
-              {sizeof("tcp://localhost:5555"), "tcp://localhost:5555"},
-              {sizeof("tcp://localhost:5556"), "tcp://localhost:5556"});
-  CZmqIpcSubscribe(zmq_ipc, {sizeof("hello"), "hello"});
-  auto *data = new uint8_t[1024 * 1024 * 100];
-  for (uint8_t idx = 0; idx < 10; idx++) {
-    // 获取当前时间
-    auto send_time = std::chrono::system_clock::now();
-    // 将时间转换为秒数
-    auto send_time_in_seconds = std::chrono::duration_cast<std::chrono::seconds>(
-        send_time.time_since_epoch()).count();
-    // 将秒数存储为字节数组
-    std::memcpy(data, &send_time_in_seconds, sizeof(uint64_t));
+  // 注册信号处理函数
+  signal(SIGINT, SignalHandler);
 
-    CZmqIpcPublish(zmq_ipc,
-                   {sizeof("hello"), "hello"},
-                   {sizeof(data), data});
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    printf("%03d -> ", idx);
-  }
-  delete[]data;
-#endif
+  ZmqIpc zmq_ipc;
 
-  CZmqIpcDel(zmq_ipc);
+  zmq_ipc.Init(CallbackFunc, 2);
+  zmq_ipc.Subscribe("hello");
+
+  while (true) {
+    if (exit_flag) {
+      break;
+    } else {
+      std::this_thread::sleep_for(std::chrono::seconds(2));
+    }
+  }
 }
--- unit_test/server.cpp
+++ unit_test/server.cpp
@@ -1,5 +1,44 @@
-#include <iostream>
+#include "zmq_ipc.h"
+
+#include <cstring>
+#include <csignal>
+
+static bool exit_flag = false;
+
+// 信号处理函数
+void SignalHandler(int signal) {
+  if (signal == SIGINT) {
+    printf("Ctrl+C pressed. Exiting...\n");
+    // 在这里执行退出操作
+    exit_flag = true;
+  }
+}
 
 int main() {
+  // 注册信号处理函数
+  signal(SIGINT, SignalHandler);
 
+  ZmqIpc zmq_ipc;
+
+  zmq_ipc.Init();
+  zmq_ipc.Publish("hello", {'w', 'o', 'r', 'l', 'd'});
+
+  while (true) {
+    if (exit_flag) {
+      break;
+    }
+
+    std::vector<uint8_t> data(1024 * 1024 * 100);
+    // 获取当前时间
+    auto time_point = std::chrono::system_clock::now();
+    // 将时间转换为ms
+    auto send_time_in_ms = std::chrono::duration_cast<std::chrono::milliseconds>(
+        time_point.time_since_epoch()
+    ).count();
+    // 将时间存储为字节数组
+    std::memcpy(data.data(), &send_time_in_ms, sizeof(uint64_t));
+
+    zmq_ipc.Publish("hello", data);
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+  }
 }
